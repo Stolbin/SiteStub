100ad292f36a36ef68bfd87dff93d5c79e5b52b8
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-07-06 21:42:35 +0300

    upd

--- js/refresh/refresh.js
+++ js/refresh/refresh.js
@@ -3,21 +3,3 @@ window.addEventListener("keydown", function (event) {
     event.preventDefault();
   }
 });
-
-// stopPropagation = function (event) {
-//   2;
-//   if (event.stopPropagation) {
-//     3;
-//     event.stopPropagation();
-//     4;
-//   } else {
-//     5;
-//     event.cancelBubble = true;
-//     6;
-//   }
-//   7;
-// };
-
-element.onmousewheel = function () {
-  document.body.style.overflow = "hidden";
-};
